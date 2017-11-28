499ccd2672a3a92f283033568a313dd666f93663
Dmitriy Morozov <Dmitriy.Morozov@ecomua.priv>
2017-11-28 15:32:02 +0200

    Added IntentService

--- app/src/main/java/com/example/dmitriymorozov/hometask1/MainActivity.java
+++ app/src/main/java/com/example/dmitriymorozov/hometask1/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.dmitriymorozov.hometask1;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +17,7 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MyLogs MainActivity";
 
     @BindView(R.id.image_main_slide)
     ImageView mSlideImage;
@@ -32,13 +35,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        mSlideshowButton.setTag(false);
+
         Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.cat1);
         Glide.with(this).load(uri).into(mSlideImage);
-        mSlideshowButton.setTag(false);
     }
 
     @OnClick(R.id.btn_main_previous)public void previous(){
         Toast.makeText(this, "previous", Toast.LENGTH_SHORT).show();
+        Intent previous = new Intent(this, BackgroundIntentService.class);
+
+        Uri uri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.cat1);
+        previous.putExtra(BackgroundIntentService.EXTRA_CURRENT_IMAGE, uri.toString());
+        previous.setAction(BackgroundIntentService.ACTION_IMAGE_PREVIOUS);
+        startService(previous);
     }
 
     @OnClick(R.id.btn_main_next)public void next(){
@@ -51,12 +61,18 @@ public class MainActivity extends AppCompatActivity {
         } else{
             isPlaying = (boolean)view.getTag();
         }
+        isPlaying = !isPlaying;
+        view.setTag(isPlaying);
 
-        view.setTag(!isPlaying);
+        Intent slideShow = new Intent(this, BackgroundIntentService.class);
         if(isPlaying){
-            view.setBackground(this.getResources().getDrawable(android.R.drawable.ic_media_play));
-        } else{
             view.setBackground(this.getResources().getDrawable(android.R.drawable.ic_media_pause));
+            slideShow.setAction(BackgroundIntentService.ACTION_SLIDESHOW_START);
+            this.startService(slideShow);
+        } else{
+            view.setBackground(this.getResources().getDrawable(android.R.drawable.ic_media_play));
+            slideShow.setAction(BackgroundIntentService.ACTION_SLIDESHOW_STOP);
+            this.stopService(slideShow);
         }
     }
 
